6ea3c2df45f065f84a147e70d0c049d6acf44d5d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-23 12:06:55 +0900

    :wrench: Run script for apply typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,71 +1,106 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-
+import commonjs from "@rollup/plugin-commonjs";
+import livereload from "rollup-plugin-livereload";
+import resolve from "@rollup/plugin-node-resolve";
+import svelte from "rollup-plugin-svelte";
+import sveltePreprocess from "svelte-preprocess";
+import {terser} from "rollup-plugin-terser";
+import typescript from "@rollup/plugin-typescript";
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
-	},
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('public/build/bundle.css');
-			}
-		}),
+    "input": "src/main.ts",
+    "output": {
+        "file": "public/build/bundle.js",
+        "format": "iife",
+        "name": "app",
+        "sourcemap": true
+    },
+    "plugins": [
+        svelte({
+            // Enable run-time checks when not in production
+            "css": (css) => {
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
-		commonjs(),
+                css.write("public/build/bundle.css");
 
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
+            },
+            "dev": !production,
 
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
+            "preprocess": sveltePreprocess()
 
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
+            /*
+             * We'll extract any component CSS out into
+             * A separate file - better for performance
+             */
+
+        }),
+
+        /*
+         * If you have external dependencies installed from
+         * Npm, you'll most likely need these plugins. In
+         * Some cases you'll need additional configuration -
+         * Consult the documentation for details:
+         * https://github.com/rollup/plugins/tree/master/packages/commonjs
+         */
+        resolve({
+            "browser": true,
+            "dedupe": ["svelte"]
+        }),
+        commonjs(),
+        typescript({"sourceMap": !production}),
+
+        /*
+         * In dev mode, call `npm run start` once
+         * The bundle has been generated
+         */
+        !production && serve(),
+
+        /*
+         * Watch the `public` directory and refresh the
+         * Browser on changes when not in production
+         */
+        !production && livereload("public"),
+
+        /*
+         * If we're building for production (npm run build
+         * Instead of npm run dev), minify
+         */
+        production && terser()
+    ],
+    "watch": {
+        "clearScreen": false
+    }
 };
 
-function serve() {
-	let started = false;
+function serve () {
 
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
+    let started = false;
 
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-					stdio: ['ignore', 'inherit', 'inherit'],
-					shell: true
-				});
-			}
-		}
-	};
+    return {
+        writeBundle () {
+
+            if (!started) {
+
+                started = true;
+
+                require("child_process").spawn(
+                    "npm",
+                    [
+                        "run",
+                        "start",
+                        "--",
+                        "--dev"
+                    ],
+                    {"shell": true,
+                        "stdio": [
+                            "ignore",
+                            "inherit",
+                            "inherit"
+                        ]}
+                );
+
+            }
+
+        }
+    };
+
 }
